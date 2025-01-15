f52ea47f95ff27970e0b22a5cb54c3d28f713451
grant-gores <140012939+grant-gores@users.noreply.github.com>
2025-01-14 19:53:15 -0600

    Add Hover effect to grid
    Used "mouseenter" in an event listener to create
    the effect of a etch-a-sketch when a div is hovered
    over.

--- script.js
+++ script.js
@@ -3,8 +3,17 @@ const container = document.querySelector("#container");
 
 for(i=0;i<256;i++){
     const div = document.createElement("div");
-    div.innerText = "hello";
-    div.style.backgroundColor = "gray";
-    // div.style.color = "gray";
+    // div.innerText = `${i}`;
+    div.classList.add("content");
     container.appendChild(div);
 }
+
+// Add "Hover" effect to grid
+const divSelector = document.querySelectorAll(".content");
+
+divSelector.forEach((div)=>{
+    // add event listener
+    div.addEventListener("mouseenter", () =>{
+        div.style.backgroundColor = "blue";
+    });
+});
